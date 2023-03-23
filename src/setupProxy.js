2686f98305d7c07508e497999fd558b9ca57da78
kakeru-private <rkeka4855@gmail.com>
2023-03-23 20:42:34 +0900

    proxy

---
+++ src/setupProxy.js
@@ -0,0 +1,13 @@
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
+module.exports = (app) => {
+	app.use(
+		'/api',
+		createProxyMiddleware({
+			target:API,
+			changeOrigin: true,
+		})
+	);
+};
+
+/*/api/* */
