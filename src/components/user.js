375404c6ec17fdfc8583f41d1c70fac8e90af445
kakeru-private <rkeka4855@gmail.com>
2023-03-30 15:18:54 +0900

    reducer

--- src/components/user.js
+++ src/components/user.js
@@ -7,17 +7,15 @@ export const user = createSlice({
         username:null           
     },
     reducers:{
-        signin(state,action) {
-            state.push({
-                uid:action.payload.uid,
-                username:action.payload.username
-            })
+        signin:(state,action) =>{
+            
+            uid=action.payload.uid,
+            username=action.payload.username
+            
         },
-        signout(state){
-            state.push({
-                uid:null,
-                username:null
-            })
+        signout:(state)=>{
+            uid=null,
+            username=null    
         }
     }
 });
