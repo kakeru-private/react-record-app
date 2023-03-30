cde46c94f729e80f95fe31fb511140018ee0128b
kakeru-private <rkeka4855@gmail.com>
2023-03-30 21:55:35 +0900

    uid

--- src/components/user.js
+++ src/components/user.js
@@ -8,7 +8,7 @@ export const user = createSlice({
     },
     reducers:{
         signin:(state,action) =>{
-            console.log(action.payload)
+            console.log(action.payload.uid)
             const uid=action.payload.uid
             const username=action.payload.username
             
